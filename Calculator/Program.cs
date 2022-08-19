81f6c0c93ef32acebcccd78565e7e23be6393232
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-19 05:06:08 -0700

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ahmadCalculator; 
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Console.Write("Enter a number ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter an operator ");
+        string op = Console.ReadLine();
+        Console.Write("Enter a second number ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        if (op == "+")
+        {
+            Console.WriteLine(num1 + num2);
+        }
+        else if (op == "-")
+        {
+            Console.WriteLine(num1 - num2);
+        }
+        else if (op == "*")
+        {
+            Console.WriteLine(num1 * num2);
+        }
+        else if (op == "/")
+        {
+            Console.WriteLine(num1 / num2);
+        }
+        else
+        {
+            Console.WriteLine("Math Error");
+        }
+            
+    }
+}
+
